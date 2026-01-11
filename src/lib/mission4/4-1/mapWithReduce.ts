7184206ae9e2fe606ad90c39da6526f8fe3a0d04
DearYuto <reasons@kakao.com>
2026-01-11 16:49:01 +0900

    refactor(mapWithReduce): AI 코드 리뷰 후 타이핑 수정버전 로직 추가

--- src/lib/mission4/4-1/mapWithReduce.ts
+++ src/lib/mission4/4-1/mapWithReduce.ts
@@ -8,3 +8,14 @@ export const mapWithReduce = <T, R>(array: Array<T>, fn: (target: T) => R) => {
     return acc;
   }, []);
 };
+
+/**
+ * AI에게 코드 리뷰 받은 후 타이핑 수정
+ */
+const mapWithReduce2 = <T, R>(array: Array<T>, fn: (target: T) => R) => {
+  return array.reduce<R[]>((acc, cur) => {
+    const newValue = fn(cur);
+    acc.push(newValue);
+    return acc;
+  }, []);
+};
